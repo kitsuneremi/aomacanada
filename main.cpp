64c1cc614e08d48f387e9f155c842cab39c035ab
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2022-11-13 22:31:19 +0700

    trả airpod đê

--- main.cpp
+++ main.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 #include <stdio.h>
 // bài 1
-//int main (){
-//    int n;
-//    printf ("nhap so nguyen : ");scanf("%d",&n);
-//    if (n<0){
-//        if(n%2==0){
-//            printf ("\nsố vừa nhập là số chẵn");
-//            printf ("\nsố vừa nhập là số nguyên âm");
-//        }else{
-//            printf ("\nsố vừa nhập là số lẻ");
-//            printf ("\nsố vừa nhập là số nguyên âm");
-//        }
-//    }else{
-//        if(n%2==0){
-//            printf ("\nsố vừa nhập là số chẵn");
-//            printf ("\nsố vừa nhập là số nguyên dương");
-//        }else{
-//            printf ("\nsố vừa nhập là số lẻ");
-//            printf ("\nsố vừa nhâp là số nguyên dương");
-//        }
-//    }
-//}
-
 int main (){
+    int n;
+    printf ("nhap so nguyen : ");scanf("%d",&n);
+    if (n<0){
+        if(n%2==0){
+            printf ("\nsố vừa nhập là số chẵn");
+            printf ("\nsố vừa nhập là số nguyên âm");
+        }else{
+            printf ("\nsố vừa nhập là số lẻ");
+            printf ("\nsố vừa nhập là số nguyên âm");
+        }
+    }else{
+        if(n%2==0){
+            printf ("\nsố vừa nhập là số chẵn");
+            printf ("\nsố vừa nhập là số nguyên dương");
+        }else{
+            printf ("\nsố vừa nhập là số lẻ");
+            printf ("\nsố vừa nhâp là số nguyên dương");
+        }
+    }
+}
+
+// bài 2
+void bai2 (){
     float a,b;
     printf("Nhap vao so a:");scanf("%f", &a);
     printf("Nhap vao so b:");scanf("%f", &b);
@@ -37,20 +38,20 @@ int main (){
 }
 
 
-// bai 3
-//int main (){
-//    float a,b,c,d;
-//    printf("Nhap vao so a:");scanf("%f", &a);
-//    printf("Nhap vao so b:");scanf("%f", &b);
-//    printf("Nhap vao so c:");scanf("%f", &c);
-//    printf("Nhap vao so d:");scanf("%f", &d);
-//    if (a>b,a>c,a>d){
-//        printf ("so lon nhat la %f",a);
-//    }else if(b>a,b>c,b>d){
-//        printf ("so lon nhat la %f",b);
-//    }else if(c>a,c>b,c>d){
-//        printf ("so lon nhat la %f",c);
-//    }else if (d>a,d>b,d>c){
-//        printf ("so lon nhat la %f",d);
-//    }
-//}
+// bài 3
+void bai3 (){
+    float a,b,c,d;
+    printf("Nhap vao so a:");scanf("%f", &a);
+    printf("Nhap vao so b:");scanf("%f", &b);
+    printf("Nhap vao so c:");scanf("%f", &c);
+    printf("Nhap vao so d:");scanf("%f", &d);
+    if (a>b,a>c,a>d){
+        printf ("so lon nhat la %f",a);
+    }else if(b>a,b>c,b>d){
+        printf ("so lon nhat la %f",b);
+    }else if(c>a,c>b,c>d){
+        printf ("so lon nhat la %f",c);
+    }else if (d>a,d>b,d>c){
+        printf ("so lon nhat la %f",d);
+    }
+}
